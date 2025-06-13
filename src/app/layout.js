98d8856c6db851b373b5204ac280dd502659d171
Bruno Gendron <bruno.gendron.consult@gmail.com>
2025-06-13 11:38:07 -0400

    wip

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 
 const RootLayout = async ({ children }) => {
   return (
-    <html lang='fr' suppressHydrationWarning>
+    <html lang={'fr'} suppressHydrationWarning>
       <body>
         <Provider system={system}>
           <Toaster />
