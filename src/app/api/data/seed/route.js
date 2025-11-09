846985472642a608de3237b11da5279bfce1529f
Bruno Gendron <bruno.gendron.consult@gmail.com>
2025-11-09 11:12:10 -0500

    wip

---
+++ src/app/api/data/seed/route.js
@@ -0,0 +1,14 @@
+const loader = createLoader(searchParams, { urlKeys })
+
+const GET = async (request) => {
+  const { nextUrl: { searchParams } } = request
+
+  // const params = loader(searchParams)
+  // const events = await getEvents(params)
+
+  return Response.json({ test: 'test '})
+}
+
+export {
+  GET
+}
