02db979c55d1160c677e3a8bb9f2be07e89d7b0e
Bruno Gendron <bruno.gendron.consult@gmail.com>
2025-11-25 09:22:08 -0500

    wip

--- src/netlify/functions/mapaq.js
+++ src/netlify/functions/mapaq.js
@@ -4,15 +4,12 @@ import insertData from '@/app/api/scheduled/mapaq/insert-data'
 import sendEmailConfirmation from '@/app/api/scheduled/mapaq/send-confirmation'
 
 const handler = async (req) => {
-    // const { next_run } = await req.json()
-    // console.log("Received event! Next invocation at:", next_run)
-
     const ftpResult = await getDataFromFtp()
+
     if (ftpResult.error) {
         console.debug(ftpResult.error.detail)
-        // sendEmailConfirmation(null, ftpResult.error.detail)
+        sendEmailConfirmation(null, ftpResult.error.detail)
         return
-        // return Response.json({ status: 'error', error: ftpResult.error.detail })
     }
 
     const data = await readCsv(ftpResult.file)
@@ -22,5 +19,5 @@ const handler = async (req) => {
 export default handler
 
 export const config = {
-    schedule: "*/5 * * * *"
+    schedule: "0 * * * *"
 }
