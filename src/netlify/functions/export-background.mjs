2229a89305f77b282f1d32d0231afc3d7f93004b
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-01-14 17:46:19 -0500

    wip

--- src/netlify/functions/export-background.mjs
+++ src/netlify/functions/export-background.mjs
@@ -60,6 +60,31 @@ async function streamExcelFile() {
   return filePath
 }
 
+const uploadToNetlify = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    const fileStream = fs.createReadStream(filePath)
+
+    fileStream.on('open', async () => {
+      try {
+
+        const fileName = path.basename(filePath)
+        const webStream = Readable.toWeb(fileStream)
+
+        const store = getStore('data-export', { siteID: '58d1d99c-beda-4a8f-b979-02f061f72ec4' })
+        await store.set(fileName, webStream)
+
+        console.log('File successfully uploaded to store')
+
+        resolve()
+      } catch (err) {
+        reject(err)
+      }
+    });
+
+    fileStream.on('error', reject)
+  })
+}
+
 const handler = async (req, context) => {
   const { url: raw } = req
   
@@ -67,59 +92,11 @@ const handler = async (req, context) => {
   const { searchParams } = url
 
   const params = loader(searchParams)
-  // console.debug('Export', params)
-
-  // const store = getStore('data-export', { siteID: '58d1d99c-beda-4a8f-b979-02f061f72ec4' })
-  // const { modified } = await store.setJSON('test', params)
-  // const { modified } = await store.setJSON('excel.xlsx', params)
 
   const filePath = await streamExcelFile()
-  console.debug(filePath)
-
-  const fileName = path.basename(filePath)
-  const fileStream = fs.createReadStream(filePath)
-  const webStream = Readable.toWeb(fileStream)
-
-  const store = getStore('data-export', { siteID: '58d1d99c-beda-4a8f-b979-02f061f72ec4' })
-  await store.set(fileName, webStream)
+  await uploadToNetlify(filePath)
 
   return Response.json({ status: 'ok' })
 }
 
 export default handler
-
-// export const config = {
-//   path: '/data/export'
-// }
-
-
-// // Check that the request is a POST method
-//   if (event.httpMethod !== 'POST') {
-//     return {
-//       statusCode: 405, // Method Not Allowed
-//       body: 'Method Not Allowed',
-//       headers: { 'Allow': 'POST' }
-//     };
-//   }
-
-//   // The request body is available as event.body (a string)
-//   const bodyString = event.body;
-
-//   try {
-//     // Parse the JSON string into a JavaScript object
-//     const data = JSON.parse(bodyString);
-
-//     // Use the data as needed
-//     console.log('Received data:', data);
-
-//     return {
-//       statusCode: 200,
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ message: 'Success', receivedData: data })
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 400,
-//       body: JSON.stringify({ message: 'Error parsing JSON body' })
-//     };
-//   }
