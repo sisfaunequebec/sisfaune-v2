0f121b77d0710251f31ad47b5ef019123c9d00db
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-01-16 09:41:36 -0500

    wip

--- src/netlify/functions/export-background.mjs
+++ src/netlify/functions/export-background.mjs
@@ -11,13 +11,12 @@ import { getStore } from '@netlify/blobs'
 
 import cursorStreamExtension from '@/lib/data/stream-extension'
 
-// import orm from '../../lib/data/database'
-
 import { searchParams, urlKeys } from '@/lib/data/events/get-events.params'
 
 import {
   createLoader
 } from 'nuqs/server'
+import { RiTreasureMapFill } from 'react-icons/ri'
 
 const loader = createLoader(searchParams, { urlKeys })
 const prisma = new PrismaClient()
@@ -26,10 +25,19 @@ const orm = prisma.$extends(cursorStreamExtension)
 async function streamExcelFile() {
 
   const dataStream = orm.event.cursorStream({
-    select: {
-      id: true,
-      typeId: true,
-      programId: true
+    include: {
+      type: true,
+      reportOrigin: true,
+      program: true,
+      status: true,
+      submitter: true,
+      discoverer: true,
+      collaborator: true,
+      habitatType: true,
+      lab: true,
+      labResponsible: true,
+      labShippingMethod: true,
+      createdBy: true,
     }
   })
 
@@ -38,7 +46,6 @@ async function streamExcelFile() {
 
   console.log(`Streaming to temp file: ${filePath}`)
 
-  // 1. Initialize the workbook writer with a file path
   const options = {
     filename: filePath,
     useStyles: true,
@@ -46,34 +53,34 @@ async function streamExcelFile() {
   }
   
   const workbook = new ExcelJS.stream.xlsx.WorkbookWriter(options)
-  const worksheet = workbook.addWorksheet('Main Report')
 
-  // 2. Define Columns
-  worksheet.columns = [
-    { header: 'ID', key: 'id'},
-    { header: 'Type', key: 'typeId' },
-    { header: 'Program', key: 'programId' }
+  const eventsWorksheet = workbook.addWorksheet('Événements')
+  const specimensWorksheet = workbook.addWorksheet('Spécimens')
+  const resultsWorksheet = workbook.addWorksheet('Résultats d\'analyses')
+
+  eventsWorksheet.columns = [
+    { header: 'Numéro d\événement', key: 'id'},
+    // { header: 'Type', key: 'typeId' },
+    // { header: 'Provenance du signalement', key: 'typeId' },
+    // { header: 'Programme', key: 'programId' },
+    // { header: 'Statut', key: 'programId' },
+    { header: 'Numéro SILAB', key: 'silabId'},
+    { header: 'Numéro MAPAQ', key: 'mapaqId'},
+    { header: 'Numéro incident CQSAS', key: 'cqsasIncidentNumber'},
+    // { header: 'Soumissionnaire', key: 'programId' },
+    // { header: 'Découvreur', key: 'programId' },
+    // { header: 'Date de découverte', key: 'programId' },
+    // { header: 'Date du signalement', key: 'programId' },
+    // { header: 'Date du fermeture', key: 'programId' },
+    { header: 'Contact avec des humains', key: 'hadHumanContact'},
+    { header: 'Contact avec des animaus', key: 'hadAnimalContact'},
   ]
 
   for await (const event of dataStream) {
-    // console.log('Adding event row...')
-    // const { id, typeId, programId } = event 
-    worksheet.addRow(event).commit()
+    const { id, silabId, mapaqId, cqsasIncidentNumber, hadHumanContact, hadAnimalContact } = event
+    eventsWorksheet.addRow(event).commit()
   }
 
-  // // 3. Simulate a massive data source (e.g., a Database Cursor or ReadStream)
-  // for (let i = 1; i <= 100000; i++) {
-  //   const rowData = {
-  //     id: i,
-  //     name: `User_${i}`,
-  //     timestamp: new Date().toISOString()
-  //   }
-
-  //   // Add row and commit it to the stream immediately
-  //   worksheet.addRow(rowData).commit();
-  // }
-
-  // 4. Finalize the workbook
   await workbook.commit()
   console.log('Excel file successfully streamed to disk')
 
@@ -87,8 +94,6 @@ const uploadToNetlify = async (filePath) => {
       siteID: '58d1d99c-beda-4a8f-b979-02f061f72ec4' 
     })
 
-    // Read the entire file into memory as a Buffer
-    // This avoids the "disturbed or locked" stream error entirely
     const fileBuffer = await readFile(filePath)
 
     await store.set(fileName, fileBuffer)
@@ -98,28 +103,6 @@ const uploadToNetlify = async (filePath) => {
     console.error('Upload failed:', err)
     throw err
   }
-  // return new Promise((resolve, reject) => {
-  //   const fileStream = fs.createReadStream(filePath)
-  //   const webStream = Readable.toWeb(fileStream)
-
-  //   fileStream.on('open', async () => {
-  //     try {
-
-  //       const fileName = path.basename(filePath)
-
-  //       const store = getStore('data-export', { siteID: '58d1d99c-beda-4a8f-b979-02f061f72ec4' })
-  //       await store.set(fileName, webStream)
-
-  //       console.log('File successfully uploaded to store')
-
-  //       resolve()
-  //     } catch (err) {
-  //       reject(err)
-  //     }
-  //   });
-
-  //   fileStream.on('error', reject)
-  // })
 }
 
 const handler = async (req, context) => {
