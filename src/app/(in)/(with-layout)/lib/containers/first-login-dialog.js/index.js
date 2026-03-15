a801716bb9693e7fe2161992514b6bf13c4bdab8
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-03-15 19:11:59 -0400

    wip

--- src/app/(in)/(with-layout)/lib/containers/first-login-dialog.js/index.js
+++ src/app/(in)/(with-layout)/lib/containers/first-login-dialog.js/index.js
@@ -7,6 +7,8 @@ import { useSWRConfig } from 'swr'
 
 import validateEmailAction from './validate-email.action'
 
+import { Alert } from '@chakra-ui/react'
+
 import BaseDialog, { Fields } from '@/app/lib/components/dialogs/base'
 
 import schema from './schema'
@@ -33,7 +35,7 @@ const FirstLoginDialog = ({ close, account }) => {
     mutate('/api/auth/user')
     // router.refresh()
     return result
-  }, [userId, router])
+  }, [userId])
 
   const fieldNames = formSchema.map(section => {
     const { fields } = section
@@ -49,12 +51,23 @@ const FirstLoginDialog = ({ close, account }) => {
   return (
     <BaseDialog 
       title={'Première connexion sur SIS Faune v2 ?'}
-      message={'Nous devons valider votre adresse de courriel et vous faire parvenir un nouveau mot de passe : veuillez saisir les informations demandées ci-dessous et cliquer sur "Envoyer".'}
+      // message={'Nous devons valider votre adresse de courriel et vous faire parvenir un nouveau mot de passe : veuillez saisir les informations demandées ci-dessous et cliquer sur "Envoyer".'}
       onSubmit={handleSubmit} submitBtnLabel={'Envoyer'} schema={schema} schemaType={'valibot'} defaultValues={defaultValues} watches={watchedFields}
     >
       {(contentRef, watched) => {
         return (
+          <>
+          <Alert.Root status={'warning'} mb={4}>
+            <Alert.Indicator />
+            <Alert.Content>
+              {/* <Alert.Title><strong>IMPORTANT</strong></Alert.Title> */}
+              <Alert.Description>
+                Nous devons valider votre adresse de courriel et vous faire parvenir un nouveau mot de passe : veuillez saisir les informations demandées ci-dessous et cliquer sur &quot;Envoyer&quot;
+              </Alert.Description>
+            </Alert.Content>
+          </Alert.Root>
           <Fields formSchema={formSchema} contentRef={contentRef} watched={watched} data={account} />
+          </>
         )}
       }
     </BaseDialog>
