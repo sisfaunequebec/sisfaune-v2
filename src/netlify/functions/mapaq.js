8bdab89b9d220f2a7df4d413157150916c574228
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-04-02 10:25:30 -0400

    wip

--- src/netlify/functions/mapaq.js
+++ src/netlify/functions/mapaq.js
@@ -9,19 +9,24 @@ const handler = async (req) => {
     const ftpResult = await getDataFromFtp()
 
     if (ftpResult.error) {
+        console.debug(`Error downloading ${sourceFileName}...`)
         sendEmailConfirmation(null, ftpResult.error.detail)
         return
     }
 
+    console.debug(`Reading CSV...`)
     const data = await readCsv(ftpResult.file)
 
+    console.debug(`Inserting data...`)
     const insertResult = await insertData(data)
 
     if (insertResult.error) {
+        console.debug(`Error inserting data...`)
         await sendEmailConfirmation(null, insertResult.error)
         return Response.json({ status: 'error', error: insertResult.error})
     }
 
+    console.debug(`Done inserting data (${insertedRowCount} rows inserted).`)
     const insertedRowCount = insertResult.data[5]
 
     await sendEmailConfirmation(insertedRowCount, null)
