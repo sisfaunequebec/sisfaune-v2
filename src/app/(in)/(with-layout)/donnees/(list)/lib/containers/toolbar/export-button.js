6a9b30dbdcc46f702bb99bdf7e207455bec69ec2
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-05-15 13:11:41 -0400

    wip

--- src/app/(in)/(with-layout)/donnees/(list)/lib/containers/toolbar/export-button.js
+++ src/app/(in)/(with-layout)/donnees/(list)/lib/containers/toolbar/export-button.js
@@ -24,7 +24,7 @@ const ExportButton = () => {
   const [filters] = useQueryStates(searchParams, { urlKeys })
 
   const { data: specimensCount } = useSpecimensCount(filters)
-  // console.debug('ExportDialog - filters:', specimensCount)
+  console.debug('ExportButton - specimensCount:', specimensCount)
 
   const { ask: confirmDownload, dialog: downloadEventsDialog } = useDialog(ExportDialog)
 
