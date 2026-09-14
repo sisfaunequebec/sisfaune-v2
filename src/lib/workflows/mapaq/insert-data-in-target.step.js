755015af53aa8d4d89ccc2efd5b084679cf0dd94
Bruno Gendron <bruno.gendron.consult@gmail.com>
2026-09-14 18:44:43 -0400

    wip

--- src/lib/workflows/mapaq/insert-data-in-target.step.js
+++ src/lib/workflows/mapaq/insert-data-in-target.step.js
@@ -12,7 +12,7 @@ const insertDataInTarget = async (data) => {
 
   if (error) {
     console.debug(`Error inserting data...`, JSON.stringify(error))
-    // await sendEmailConfirmation(null, error)
+    await sendEmailConfirmation(null, error)
     throw new FatalError(error)
   }
 
